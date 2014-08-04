4d10e78410f37c3d08047d7b5eed11f81f76fb5a
Bao Nguyen <bao@ooyala.com>
2014-08-04 14:45:48 -0700

    updating email and adding LICENSE

--- test/cookbooks/quagga_test/metadata.rb
+++ test/cookbooks/quagga_test/metadata.rb
@@ -1,6 +1,6 @@
 name             "quagga-test"
 maintainer       "Bao Nguyen"
-maintainer_email "ngqbao@gmail.com"
+maintainer_email "opensource-cookbooks@ooyala.com"
 license          "All rights reserved"
 description      "Generic Quagga cookbook testing"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
